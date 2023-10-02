d4f69a04d42372472c1808062977207f5e1e722f
danielfais <143370849+danielfais@users.noreply.github.com>
2023-10-02 18:06:05 +0700

    Update main.js

--- menu/main.js
+++ menu/main.js
@@ -1,10 +1,1 @@
-var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
-var menu = document.getElementsByClassName('menu')[0];
 
-tombolMenu.onclick = function() {
-    menu.classList.toggle('active');
-}
-
-menu.onclick = function() {
-    menu.classList.toggle('active');
-}
